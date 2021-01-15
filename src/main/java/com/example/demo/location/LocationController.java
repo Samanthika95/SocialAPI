5cd306e543580b96691cca95c4d7ecb5720b8dce
Samanthika Rajapaksha <53019782+samaa9504@users.noreply.github.com>
2021-01-15 14:55:32 +0530

    Get List of posts and locations

--- src/main/java/com/example/demo/location/LocationController.java
+++ src/main/java/com/example/demo/location/LocationController.java
@@ -1,8 +1,22 @@
 package com.example.demo.location;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LocationController {
+	
+	@RequestMapping(value = "/locations")
+	public List<Location> getAllLocations(){
+		
+		Location location1 = new Location("l1","Lagos");
+		Location location2 = new Location("l2", "Asaba");
+		Location location3 = new Location("l3", "Budapest");
+		return Arrays.asList(null);
+		
+	}
 
 }
