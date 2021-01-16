d60abfa51794c96b725e6980866e9756c9e43e82
Samanthika Rajapaksha <53019782+samaa9504@users.noreply.github.com>
2021-01-16 15:25:26 +0530

    get location and posts by is

--- src/main/java/com/example/demo/location/LocationController.java
+++ src/main/java/com/example/demo/location/LocationController.java
@@ -3,20 +3,26 @@ package com.example.demo.location;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LocationController {
 	
+	@Autowired
+	private LocationService locationservice;
+	
 	@RequestMapping(value = "/locations")
 	public List<Location> getAllLocations(){
+		return locationservice.getAllLocations(); 
 		
-		Location location1 = new Location("l1","Lagos");
-		Location location2 = new Location("l2", "Asaba");
-		Location location3 = new Location("l3", "Budapest");
-		return Arrays.asList(location1,location2,location3);
-		
+	}
+	
+	@RequestMapping(value = "/locations/{id}")
+	public Location getLocation(@PathVariable String id) {
+		return locationservice.getLocation(id);
 	}
 
 }
--- src/main/java/com/example/demo/location/LocationService.java
+++ src/main/java/com/example/demo/location/LocationService.java
@@ -1,8 +1,29 @@
 package com.example.demo.location;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class LocationService {
+	Location location1 = new Location("l1","Lagos");
+	Location location2 = new Location("l2", "Asaba");
+	Location location3 = new Location("l3", "Budapest");
+	
+	public List<Location> locations = Arrays.asList(location1,location2,location3);
+	
+	public List<Location> getAllLocations(){
+		return locations;
+	}
+	
+	 public Location getLocation(String id) {
+		 Location location = locations.stream()
+				 .filter(t -> id.equals(t.getId()))
+				 .findFirst()
+				 .orElse(null);
+		 return location;
+				 
+	 }
 
 }
