346bda6f501176975e6b9603b5a7b0ea617e8df1
Samanthika Rajapaksha <53019782+samaa9504@users.noreply.github.com>
2021-01-17 11:50:10 +0530

    insert users and posts

--- src/main/java/com/example/demo/user/UserController.java
+++ src/main/java/com/example/demo/user/UserController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -25,5 +27,10 @@ public class UserController {
 		public User getUser(@PathVariable String id) {
 			return userService.getUser(id);
 		}
+	    
+	    @RequestMapping(value = "/users" ,method = RequestMethod.POST)
+	    public void addUser(@RequestBody User user) {
+	    	userService.addUser(user);
+	    }
 
 }
