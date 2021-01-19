ed5d8852d15269b1d06c577979657f6e8ab23706
Samanthika Rajapaksha <53019782+samaa9504@users.noreply.github.com>
2021-01-19 21:54:00 +0530

    Student - Request Mapping

--- src/main/java/com/example/demo/student/studentService.java
+++ src/main/java/com/example/demo/student/studentService.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class studentService {
+public class StudentService {
 	
 	@Autowired
 	private StudentRepository  studentRepository;
