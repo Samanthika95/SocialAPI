8029eb1650487029458cb5f42f2999d9852e6cd6
Samanthika Rajapaksha <53019782+samaa9504@users.noreply.github.com>
2021-01-29 22:01:20 +0530

    Remove student

--- src/main/java/com/example/demo/student/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.student;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface StudentRepository extends CrudRepository<Student, String> {
-	
-	// With this the crud methods are now available
-}
